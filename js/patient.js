64a5a456c118fbc5430bc3931aba28886a4bf1be
JuanManuel1207 <juan.lopez14@uptc.edu.co>
2022-06-30 11:57:20 -0500

    CRUD DOCTOR FULL

--- js/patient.js
+++ js/patient.js
@@ -13,7 +13,7 @@ let getTable = async ()=> {
             <td>${data.eps}</td>
             <td>${(data.gender==true?"Femenino":"Masculino")}</td>
             <td >
-                <a id="btnUpd" class="btn btn-sm btn-warning bi bi-pencil" onclick="updPatient('${data._id}')" ></a>
+                <a id="btnUpd" class="btn btn-sm btn-warning bi bi-pencil" data-toggle="modal" data-target="#updateModal" onclick="updPatient('${data._id}')"></a>
                 <a id="btnDel" class="btn btn-sm btn-danger bi bi-x-lg" onclick="delPatient('${data._id}')"></a>
             </td> 
         </tr>
@@ -30,7 +30,35 @@ async function updPatient(id){
     })
     const result = await response.json()
     console.log(result.data.name)
+    document.getElementById('code').value = result.data._id
+    document.getElementById('dni').value = result.data.id
+    document.getElementById('names').value = result.data.name
+    document.getElementById('phone').value = result.data.phone
+    document.getElementById('age').value = result.data.age
+    document.getElementById('eps').value = result.data.eps
 }
+
+document.getElementById('saveChanges').addEventListener('click', async ()=>{
+
+    const response = await fetch("http://localhost:3000/patients/"+document.getElementById('code').value,{
+        method:'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "id" : document.getElementById('dni').value,
+	        "name" : document.getElementById('names').value,
+	        "phone" : document.getElementById('phone').value,
+	        "age" : document.getElementById('age').value,
+	        "eps" : document.getElementById('eps').value,
+	        "gender" : document.getElementById('gender').value
+        })
+    })
+
+    const result = await response.json()
+    result.result==true ? window.alert("Paciente Modificado Con Éxito") : window.alert("Paciente No Modificado")
+    getTable()
+})
 
 async function delPatient(id){
     const response = await fetch("http://localhost:3000/patients/"+id,{
